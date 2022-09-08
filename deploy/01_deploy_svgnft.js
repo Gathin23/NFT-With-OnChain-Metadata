5b40c8ba09c2bbe15f8d623c421b53b8dac2b46b
Gathin <gathint23@gmail.com>
2022-09-08 15:11:49 +0530

    Inital deploy script

---
+++ deploy/01_deploy_svgnft.js
@@ -0,0 +1,28 @@
+const fs = require("fs")
+const { ethers } = require("hardhat")
+const { hrtime } = require("process")
+
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId
+}) => {
+    const { deploy, log} = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = await getChainId()
+
+    log("------------------------------------")
+    const SVGNFT = await deploy("SVGNFT", {
+        from:deployer,
+        log:true
+    })
+    log(`You have deployed an NFT contract to ${SVGNFT.address}`)
+    let filepath = "./img/triangle.svg"
+    let svg = fs.readFileSync(filepath, { encoding: "utf8"})
+
+    const svgNFTContract = await ethers.getContractFactory("SVGNFT")
+    const accounts = await hrtime.ethers.getSigners()
+    const signer = accounts[0]
+    const svgNFT = new ethers.Contract(SVGNFT.address, svgNFTContract.interface, signer)
+    
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-deploy")
+require("@nomiclabs/hardhat-ethers")
+require("ethers")
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: { },
+  },
   solidity: "0.8.9",
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  }
 };
