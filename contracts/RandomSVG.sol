303b6947f1f7bba6878f0c073a1c952a0e33b62c
Gathin <gathint23@gmail.com>
2022-09-12 22:32:00 +0530

    RandomSVG oracle connnection

--- contracts/RandomSVG.sol
+++ contracts/RandomSVG.sol
@@ -2,16 +2,24 @@
 
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 contract RandomSVG is ERC721URIStorage, VRFConsumerBaseV2 {
-
-    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyHash, uint256 _fee) 
-    VRFConsumerBase(_VRFCoordinator, _LinkToken) 
-    ERC721("RandomSVG", "rsNFT") 
-    {
-        fee = _fee;
-        keyHash = _keyHash;
+    VRFCoordinatorV2Interface COORDINATOR;
+    address s_owner;
+    uint64 s_subscriptionId;
+    address vrfCoordinator;
+   
+    constructor(uint64 subscriptionId) ERC721("RandomSVG", "rsNFT") VRFConsumerBaseV2(vrfCoordinator) {
+    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    s_owner = msg.sender;
+    s_subscriptionId = subscriptionId;
     }
+    
+    function create() public {
+        
+    }
+
 }
